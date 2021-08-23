5e356038aa34d4fa824950d6933c9f192a0e7dee
treasureBear94 <qvo7896@gmail.com>
2021-08-23 23:37:24 +0900

    :sparkles: radio button

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,18 +1,20 @@
 package hello.itemservice.domain.item;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * @author yjjung
  * @version 0.1.0
  * @since 2021/08/18
  */
+@Getter
 @AllArgsConstructor
 public enum ItemType {
 
     BOOK("도서"),
     FOOD("음식"),
-    ETC("음식");
+    ETC("기타");
 
     private final String description;
 
